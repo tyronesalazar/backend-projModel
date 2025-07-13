d23b004fe4cff28348721eee945358dec41af720
Tyrone <tyronesalazar.business@outlook.es>
2025-07-13 18:28:01 -0500

    Hotfix rutas pedidos

--- routes/pedidos.routes.js
+++ routes/pedidos.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
     crearPedidoDesdeCarrito,
 } = require("../controllers/pedidos.controller");
+const verificarToken = require("../middlewares/verificarToken");
 const router = express.Router();
 
-router.post("/", crearPedidoDesdeCarrito);
+router.post("/", verificarToken, crearPedidoDesdeCarrito);
