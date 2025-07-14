c62f3b64b0864e77d7e42f6c86c0d49ec7b8d8f3
Tyrone <tyronesalazar.business@outlook.es>
2025-07-14 15:34:03 -0500

    Ruta para obtener pedidos de un usuario

--- routes/pedidos.routes.js
+++ routes/pedidos.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const {
     crearPedidoDesdeCarrito,
-    obtenerPedidoUsuario,
     obtenerPedidosEnEspera,
     obtenerPedidosEnPreparacion,
 } = require("../controllers/pedidos.controller");
@@ -14,6 +13,5 @@ const router = express.Router();
 router.post("/", verificarToken, crearPedidoDesdeCarrito);
 router.get("/", verificarCocinero, obtenerPedidosEnEspera);
 router.get("/preparacion", verificarCocinero, obtenerPedidosEnPreparacion);
-router.get("/:id", verificarToken, obtenerPedidoUsuario);
 
 module.exports = router;
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -12,6 +12,7 @@ const {
     deleteFavorite,
 } = require("../controllers/usuarios.controller");
 const { verificarToken } = require("../middlewares/verificarToken");
+const { obtenerPedidoUsuario } = require("../controllers/pedidos.controller");
 const router = express.Router();
 
 router.get("/all", getUsuarios);
@@ -24,5 +25,6 @@ router.post("/check-email", checkEmail);
 router.post("/login", loginUsuario);
 router.post("/update-password", updatePassword);
 router.get("/favorites/all", verificarToken, getFavorites);
+router.get("/pedidos/all", verificarToken, obtenerPedidoUsuario);
 
 module.exports = router;
